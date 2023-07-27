5f5e4bb9cfc27484e14a82f2e7fa962bac05f22d
Abishek2211 <140708168+Abishek2211@users.noreply.github.com>
2023-07-27 15:52:14 +0530

    Add files via upload

---
+++ NonPrimitiveDataTypes.java
@@ -0,0 +1,11 @@
+package datatypes;
+
+public class NonPrimitiveDataTypes {
+	public static void main(String args[]) {
+		String name="abishek"; //normal way to create string
+		String s=new String("raj");
+		System.out.println(name);
+		System.out.println(s);
+	}
+
+}
